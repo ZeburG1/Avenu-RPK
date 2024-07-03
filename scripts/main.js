960ec6ce283daa00243b6dd73e906d3834ffc674
Ivanov Ivan <148995309+SupZeb@users.noreply.github.com>
2024-07-03 20:25:21 +0500

    first commit

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,7 @@ logo.addEventListener('mouseenter', function() {
     logo.style.scale = 1.1;
 });
 logo.addEventListener('mouseleave', function() {
+    logo.style.transitition = "0.2s";
     logo.style.filter = "drop-shadow(0 0 0 #000)";
     logo.style.scale = 1.0;
 });
